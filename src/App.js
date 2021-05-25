0f3f7ea39cd3314c85d683cd9e08d0c0e4b571a5
shury2323 <gabrielbbrito_1@hotmail.com>
2021-05-24 22:39:20 -0300

    Fazendo chamada da api

--- src/App.js
+++ src/App.js
@@ -15,28 +15,33 @@ function App() {
   ]
 
   useEffect(() => {
+
     axios('https://accounts.spotify.com/api/token', {
-    headers: {
-      'content-type': 'application/x-www-form-urlencoded',
-      'authorization': 'Basic' + btoa(spotify.ClientId + ':' + spotify.ClientSecret)
-    },
-    data  : 'grant_type = client_credentials',
-    method : 'POST'
+      headers: {
+        'Content-Type' : 'application/x-www-form-urlencoded',
+        'Authorization' : 'Basic ' + btoa(spotify.ClientId + ':' + spotify.ClientSecret)      
+      },
+      data: 'grant_type=client_credentials',
+      method: 'POST'
     })
-    .then(tokenResponse => {
-      console.log(tokenResponse.data.access_token);
+    .then(tokenResponse => {      
       setToken(tokenResponse.data.access_token);
 
-      axios('GET https://api.spotify.com/v1/browse/categories?locale=pt_BR',{
-        method : 'GET',
-        headers: {'Authorization' : 'Bearer' + tokenResponse.data.access_token}
+      axios('https://api.spotify.com/v1/browse/categories?locale=sv_US', {
+        method: 'GET',
+        headers: { 'Authorization' : 'Bearer ' + tokenResponse.data.access_token}
       })
-      .then(genreResponse =>{
-        setGenres(genreResponse.data.categories.items)
-      })
+      .then (genreResponse => {        
+        setGenres({
+          selectedGenre: genres.selectedGenre,
+          listOfGenresFromAPI: genreResponse.data.categories.items
+        })
+      });
+      
     });
 
-  }, []);
+  }, [genres.selectedGenre, spotify.ClientId, spotify.ClientSecret]); 
+
 
   return (
     <form onSubmit = {() => {}}>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+//import $ from 'jquery';
+//import Popper from 'popper.js';
 import App from './App';
 
 
